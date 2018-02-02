8473f7b20132206a6f8917b397b721dde9fae56a
Santiago Smith <sssilvar@unal.edu.co>
2018-02-02 14:42:09 -0500

    First Step
    [01_video2frames] as non-function is added.

---
+++ 01_video2frames/lib/video2frames.m
@@ -0,0 +1,62 @@
+function video2frames( video_file, extension )
+%VIDEO2FRAMES Converts a videofile into frames
+%   video_file: path to the video
+%
+%   folder_destination: the folder where the images will be stored.
+%   IMPORTANT!: This code will create a folder inside [folder_destination]
+%   with the same name of the video
+%
+%   extension: The image output extension:
+%               jpg, tif, etc.
+
+% Extract video path and filename
+[filepath,name, ~] = fileparts(video_file);
+folder_destination = [filepath, filesep, 'muestra'];
+% folder_destination = [filepath, filesep, name];
+
+% Load video
+video = VideoReader(video_file);
+
+% Video parameters (do not modify)
+fr = video.FrameRate;
+tt = video.Duration;
+
+% Set cutting parameters
+initial_time    = 0;    % Start cutting (in seconds)
+final_time      = tt;    % Final cutting (in seconds)
+frame_step      = 1;   % It will jump between [frame_step] frames
+
+
+% Start frame extraction
+if final_time > tt
+    error('The final time is larger than the video duration.')
+elseif initial_time < 0
+    error('Initial time cannot be negative.')
+elseif isequal(folder_destination,'')
+    error('No folder has been set as a directory ouput.')
+else
+    % Clear previous files
+    if exist(folder_destination,'dir')
+        disp('Folder already exists. Deleting...')
+        rmdir(folder_destination, 's');
+        mkdir(folder_destination);
+    else
+        mkdir(folder_destination);
+    end
+    
+    
+    % Start frame extraction
+    disp('Converting video...');
+    ii = 1;
+    while hasFrame(video)
+        img = readFrame(video);
+        filename = [sprintf('%03d',ii) ['.', extension] ];
+        fullname = fullfile(folder_destination, filename);
+        imwrite(img,fullname)    % Write out to a JPEG file (img1.jpg, img2.jpg, etc.)
+        ii = ii+1;
+    end
+    disp('Conversion finished SUCCESSFULLY');
+end
+
+end
+
---
+++ 01_video2frames/video2frames_folder.m
@@ -0,0 +1,70 @@
+%% VIDEO2FRAMES
+%   This code goes over every folder looking for videos and converting them
+%   into frames.
+
+%% Add libraries and files separator (f)
+addpath(genpath('lib'));
+f = filesep;
+
+%% Set dataset folder
+dataset_folder = '/home/jullygh/Dataset_riie/All';
+
+%% Start looking for videos inside dataset_folder
+
+% Control processing
+control_folder = [dataset_folder, f, 'Controls/GoPro/'];
+control_dir = new_dir(control_folder);
+
+for i = 1:length(control_dir)
+    if ~isequal(control_dir(i).name, '.') && ~isequal(control_dir(i).name, '..')        
+
+        smooth_dir = new_dir([control_dir(i).path, f, control_dir(i).name, f, 'SMOOTH/*.MP4']);
+        saccad_dir = new_dir([control_dir(i).path, f, control_dir(i).name, f, 'SACCAD/*.MP4']);
+
+        % process SMOOTH directory
+        for j = 1:length(smooth_dir)
+            video_file = [smooth_dir(j).path, f, smooth_dir(j).name];
+            disp(['Processing: ', video_file])
+            video2frames(video_file, 'tif')
+
+        end
+
+        % process SACCAD directory
+        for j = 1:length(saccad_dir)
+            video_file = [saccad_dir(j).path, f, saccad_dir(j).name];
+            disp(['Processing: ', video_file])
+            video2frames(video_file, 'tif')
+        end
+    end
+end
+
+
+% CP processing
+cp_folder = [dataset_folder, f, 'CP/GoPro/'];
+cp_dir = new_dir(cp_folder);
+
+for i = 1:length(cp_dir)
+    if ~isequal(cp_dir(i).name, '.') && ~isequal(cp_dir(i).name, '..')
+
+        smooth_dir = new_dir([cp_dir(i).path, f, cp_dir(i).name, f, 'SMOOTH/*.MP4']);
+        saccad_dir = new_dir([cp_dir(i).path, f, cp_dir(i).name, f, 'SACCAD/*.MP4']);
+
+        % process SMOOTH directory
+        for j = 1:length(smooth_dir)
+            video_file = [smooth_dir(j).path, f, smooth_dir(j).name];
+            disp(['Processing: ', video_file])
+            video2frames(video_file, 'tif')
+
+        end
+
+        % process SACCAD directory
+        for j = 1:length(saccad_dir)
+            video_file = [saccad_dir(j).path, f, saccad_dir(j).name];
+            disp(['Processing: ', video_file])
+            video2frames(video_file, 'tif')
+
+        end
+    end
+end
+
+save([dataset_folder, filesep, 'SUCCESS'], '-ascii')
